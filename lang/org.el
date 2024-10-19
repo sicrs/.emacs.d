27da06201376fa1df31fc5cfa57563e387ade00d
sicrs <s.taharuddin@tum.de>
2024-10-19 11:31:30 +0200

    org: add custom todo capture templates and other org-related configuration settings

--- lang/org.el
+++ lang/org.el
@@ -9,8 +9,25 @@
   (setq org-preview-latex-default-process 'imagemagick)
   (setq org-latex-compiler "xelatex")
 
+  ;; location for agenda
+  (setq org-agenda-files '("~/org") ;; emacs now knows where to find the agenda
+	org-log-done 'time ;; record timestamp on completion
+	org-return-follow-link t ;; follow links using RET
+	org-hide-emphasis-markers t) ;; hide ** on bold
+
   ;; for org-modern -- this saves us the bother of calling the setq on hook
   (setq org-ellipsis "…")
+  (set-face-attribute 'org-ellipsis nil :inherit 'default :box nil)
+
+  ;; configure capture templates
+  (setq org-todo-keywords
+	'((sequence  "TODO(t)" "PLANNING(p)" "IN-PROGRESS(i@/!)" "BLOCKED(b@)" "|" "DONE(d!)" "ABORTED(w@/!)")))
+  (setq org-capture-templates
+	'(("g" "Generic to-do"
+	   entry (file+headline "~/org/todo.org" "General tasks")
+	   "* TODO [#B] %?\n:Created: %T\n "
+	   :empty-lines 0)
+	  ))
   (blackout 'visual-line-mode)
   (blackout 'org-indent-mode))
 
@@ -27,7 +44,6 @@
   (add-hook 'org-modern-mode-hook
 	    (lambda ()
 	      (setq line-spacing 0.3)))
-  (set-face-attribute 'org-ellipsis nil :inherit 'default :box nil)
   ;; edit settings
   (setq org-auto-align-tags nil
 	org-tags-column 0
