d4c5001c4b797719be9a2d86e57d92b6b9c73844
sicrs <s.taharuddin@tum.de>
2024-11-07 10:40:22 +0100

    init: bump elpaca version

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
 
 (setq native-comp-speed 3)
 ;; elpaca-loader
-(defvar elpaca-installer-version 0.7)
+(defvar elpaca-installer-version 0.8)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
