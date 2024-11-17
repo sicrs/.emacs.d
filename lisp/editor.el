b80037c20fc1e396b06f2d62d97cb5ccbf7a352c
sicrs <s.taharuddin@tum.de>
2024-11-17 20:36:20 +0100

    editor: use file-exists-p and not file-directory-p

--- lisp/editor.el
+++ lisp/editor.el
@@ -29,15 +29,14 @@
 ;; save customisations elsewhere
 (setq custom-file (expand-file-name ".custom.el" user-emacs-directory))
 
-(unless (file-directory-p custom-file)
+(unless (file-exists-p custom-file)
   (if (or (eq system-type 'darwin)
           (eq system-type 'gnu/linux))
-      ((shell-command (format "touch %s" custom-file))
-       (message ".custom.el not found, creating..."))
+      (shell-command (format "touch %s" custom-file))
     (if (eq system-type 'ms-dos)
-        ((shell-command (format "type NUL > %s" custom-file))
-         (message ".custom.el not found, creating..."))
-      (error "Unknown system!"))))
+        (shell-command (format "type NUL > %s" custom-file))
+      (error "Unknown system!")))
+  (message ".custom.el not found, creating..."))
 (load custom-file)
 
 ;; Consider using meow? but I want to maintain familiarity with vim bindings, the reverse motions don't seem interesting to me...
